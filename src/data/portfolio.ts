79529ae544aed6042e6549ee78c8483d0f0b5cb0
Mohamed <mofathy1833@gmail.com>
2026-05-03 14:18:36 +0300

    feat(cv): add Mohamed Fathy's CV as a PDF file and update portfolio data reference

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -16,7 +16,7 @@ export const personal = {
 	email: 'mofathy1833@gmail.com',
 	github: 'https://github.com/Mofathy183',
 	linkedin: 'https://www.linkedin.com/in/mohamed-fathy-8376812b5/',
-	cv: '/Mohamed_Fathy_CV.pdf',
+	cv: '/public/cv/Mohamed_Fathy_CV.pdf',
 };
 
 /**
